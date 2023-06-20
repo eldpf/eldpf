3102bef7d99957a77639c44c6dffa3b1de174ec9
eldpf <136875301+eldpf@users.noreply.github.com>
2023-06-20 18:12:15 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b && b==c)
+    {
+        cout<<10000+1000*a;
+    }
+    else if(a==b || a==c)
+    {
+        cout<<1000+a*100;
+    }
+    else if(b==c)
+    {
+        cout<<1000+b*100;
+    }
+    else
+    {
+        if(a>b && a>c)
+        {
+            cout<<a*100;
+        }
+        else if(b>a && b>c)
+        {
+            cout<<b*100;
+        }
+        else
+        {
+            cout<<c*100;
+        }
+    }
+}
