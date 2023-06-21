29f18ecd6bb56392bf69c9dc8e30ae239b1a1207
eldpf <136875301+eldpf@users.noreply.github.com>
2023-06-21 19:53:26 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=n;i>0;i--)
+    {
+        for(int k=1;k<=i;k++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
